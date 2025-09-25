c92cb26adc73cf0773a3a2de66a0e758f352b151
Luozheng Qin <qinluozheng@sais.com.cn>
2025-09-25 16:42:33 +0800

    Update run_unicot_wise_self_reflection_v0.2.sh

--- scripts/run_unicot_wise_self_reflection_v0.2.sh
+++ scripts/run_unicot_wise_self_reflection_v0.2.sh
@@ -6,10 +6,10 @@ do
         --group_id $i \
         --group_num $gpu_num \
         --mode pure_self_re \
-        --model_path "/inspire/ssd/project/sais-mtm/public/qlz/pretrained_models/BAGEL-7B-MoT" \
-        --model_fine_tuned_path "/inspire/ssd/project/sais-mtm/public/qlz/code/BagelCoT/results/0908_unicot_v2.0_v2_0908_final_ripple/checkpoints/0009000" \
+        --model_path "ByteDance-Seed/BAGEL-7B-MoT" \
+        --model_fine_tuned_path "Fr0zencr4nE/UniCoT-7B-MoT-v0.2" \
         --data_path "./eval/gen/wise/final_data.json" \
-        --output_dir "./results/further-s-1000" \
+        --output_dir "./results/" \
         --cfg_text_scale 4 \
         --cfg_img_scale 2.0 > process_log_$i.log 2>&1 &
 done
