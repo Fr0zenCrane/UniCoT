474afc5ceaa07c428a4745dfdf285d0f94badafb
Luozheng Qin <qinluozheng@sais.com.cn>
2025-09-25 16:40:24 +0800

    Update run_wise_self_reflection_v0.1.sh

--- scripts/run_wise_self_reflection_v0.1.sh
+++ scripts/run_wise_self_reflection_v0.1.sh
@@ -5,7 +5,7 @@ do
     CUDA_VISIBLE_DEVICES=$i python inference_mdp_self_reflection_wise_v0.1.py \
         --group_id $i \
         --group_num $gpu_num \
-        --model_path "Fr0zencr4nE/UniCoT-7B-MoT" \
+        --model_path "path/to/unicot" \
         --data_path "./eval/gen/wise/final_data.json" \
         --outdir "./results" \
         --cfg_text_scale 4 > process_log_$i.log 2>&1 &
