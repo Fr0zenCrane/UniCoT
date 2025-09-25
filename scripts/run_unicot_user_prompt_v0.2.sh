e90ccc1af21d2e509be47981f8a31863391e4894
Luozheng Qin <qinluozheng@sais.com.cn>
2025-09-25 16:38:47 +0800

    Update run_unicot_user_prompt_v0.2.sh

--- scripts/run_unicot_user_prompt_v0.2.sh
+++ scripts/run_unicot_user_prompt_v0.2.sh
@@ -6,10 +6,10 @@ do
         --group_id $i \
         --group_num $gpu_num \
         --mode pure_breakdown \
-        --model_path "/inspire/ssd/project/sais-mtm/public/qlz/pretrained_models/BAGEL-7B-MoT" \
-        --model_fine_tuned_path "/inspire/ssd/project/sais-mtm/public/qlz/code/BagelCoT/results/0908_unicot_v2.0_v2_0908_final_ripple/checkpoints/0009000" \
+        --model_path "ByteDance-Seed/BAGEL" \
+        --model_fine_tuned_path "path/to/unicot" \
         --data_path "./test_prompts.txt" \
-        --output_dir "./results/further-s-1000" \
+        --output_dir "./results/" \
         --cfg_text_scale 4 \
         --cfg_img_scale 2.0 \
         > process_log_$i.log 2>&1 &
