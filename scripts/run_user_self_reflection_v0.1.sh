c34393807cc73b87424f6bc4f39f45f5931b29ab
Luozheng Qin <qinluozheng@sais.com.cn>
2025-09-25 16:40:05 +0800

    Update run_user_self_reflection_v0.1.sh

--- scripts/run_user_self_reflection_v0.1.sh
+++ scripts/run_user_self_reflection_v0.1.sh
@@ -5,7 +5,7 @@ do
     CUDA_VISIBLE_DEVICES=$i python inference_mdp_self_reflection_v0.1.py \
         --group_id $i \
         --group_num $gpu_num \
-        --model_path "Fr0zencr4nE/UniCoT-7B-MoT" \
+        --model_path "path/to/unicot" \
         --data_path "./test_prompts.txt" \
         --outdir "./results" \
         --cfg_text_scale 4 > process_log_$i.log 2>&1 &
